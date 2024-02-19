44491e49cd5bf1c8efb7d5da5919d85fc943b535
Devendra Chaudhary <devendra13.dc@gmail.com>
2024-02-19 18:02:56 +0545

    ...

--- src/Pages/account/Account.jsx
+++ src/Pages/account/Account.jsx
@@ -1,22 +1,26 @@
-
 import { getPassengerProfileData } from "../../query/FirestoreQuery";
-import { Suspense, lazy, useEffect, useState } from "react";
-const ProfileData = lazy(() => import("../../components/Sections/ProfileData"));
+import { useEffect, useState } from "react";
+import ProfileData from "../../components/Sections/ProfileData";
 
 const Account = () => {
+const [data, setData] = useState()
 
+useEffect(() => {
+  getPassengerProfileData().then((res) => {
+    setData(res)
+  });
+}, [])
 
-  const data = getPassengerProfileData().then((res) => res);
-  console.log(data)
+  
+  console.log(data);
   return (
     <div className=" bg-white/75 min-h-[70vh] backdrop-blur-sm w-full p-[15px] rounded-[15px]">
       <h2 className="text-titleSize font-titleFontWeight text-titleColor ">
         {" "}
         Profile{" "}
       </h2>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ProfileData data={data} />
-      </Suspense>
+
+      <ProfileData data={data} />
     </div>
   );
 };
